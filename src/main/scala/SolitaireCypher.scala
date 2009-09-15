cb5a685e46db4053356f8039af23c7e803097f88
jamescarr <jamescarr@abidos.(none)>
2009-09-15 18:46:22 -0500

    failing X pad

--- src/main/scala/SolitaireCypher.scala
+++ src/main/scala/SolitaireCypher.scala
@@ -2,6 +2,6 @@
 class SolitaireCypher{
   def discardNonAlpha(input:String):String = {
     val cleaned = input.replaceAll("[^A-Za-z]", "").toUpperCase()
-     "(.{0,5})".r.replaceAllIn(cleaned, "$1 ").trim()
+    "(.{0,5})".r.replaceAllIn(cleaned, "$1 ").trim()
   }
 }
--- src/test/scala/SolitaireCypherSpec.scala
+++ src/test/scala/SolitaireCypherSpec.scala
@@ -3,22 +3,28 @@ import org.specs._
 object solitaireCypherSpec extends Specification {  
   var message = "Code in ruby live 2 longer!"
   val encryptor = new SolitaireCypher
-  "discard any non A-Z characters" in {     
+  "should discard any non A-Z characters" in {     
      val result = encryptor.discardNonAlpha("O>NE!T@W")
      
      result must be matching ("^[A-Za-z]+$")
   }
   
-  "uppercase all characters" in {
+  "should uppercase all characters" in {
      val result = encryptor.discardNonAlpha("ONETW")
      
      result must be matching ("^[A-Z]+$")
   }
   
-  "put a space after each fifth character" in {
+  "should put a space after each fifth character" in {
       val result = encryptor.discardNonAlpha(message)
       
       result must be matching("CODEI NRUBY LIVEL ONGER")
+  }
+  
+  "should pad remaining space with Xs" in {
+      val result = encryptor.discardNonAlpha("Code in scala live forever")
+      
+      result must be matching("CODEI NSCAL ALIVE FOREV ERXXX")
   }
   
 }
