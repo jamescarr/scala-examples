a1ae61e5e327b055b5843facf02f322893448505
jamescarr <jamescarr@abidos.(none)>
2009-09-15 21:11:39 -0500

    passing specs

--- src/main/scala/SolitaireCypher.scala
+++ src/main/scala/SolitaireCypher.scala
@@ -1,7 +1,11 @@
 
 class SolitaireCypher{
   def discardNonAlpha(input:String):String = {
-    val cleaned = input.replaceAll("[^A-Za-z]", "").toUpperCase()
-    "(.{0,5})".r.replaceAllIn(cleaned, "$1 ").trim()
+    val cleaned = input.replaceAll("[^A-Za-z]", "").toUpperCase().trim()
+    var result = cleaned + ("X" * numberOfXs(cleaned))
+    "(.{0,5})".r.replaceAllIn(result, "$1 ").trim()
+  }
+  private def numberOfXs(s:String):Int={
+    return if(s.length % 5 == 0) 0 else s.length % 5 +1
   }
 }
